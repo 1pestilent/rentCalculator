7536f6822bdcdece9eda67c2c4940aa9039cd923
Mikhail <1pestilent@gmail.com>
2024-03-08 11:58:16 +0500

    Добавлена возможность создание записей о машинах и прибыли

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import QDate
 from PySide6.QtSql import QSqlDatabase, QSqlQuery
+import sqlite3
 
 class ConnectToDB:
     def __init__(self):
@@ -19,7 +20,7 @@ class ConnectToDB:
                         CREATE TABLE IF NOT EXISTS cars(
                             id INTEGER PRIMARY KEY AUTOINCREMENT,
                             date_of_added DATE,
-                            name TEXT)
+                            name TEXT UNIQUE)
                           """):
             return False
         else:
@@ -39,7 +40,65 @@ class ConnectToDB:
             print('Table with income was created!')
         
         return True
+    
+    def add_car_in_db(self,carname):
+        current_date = QDate.currentDate()
+        print(current_date.toString('yyyy-MM-dd'))
+        query = QSqlQuery()
+        query.prepare('INSERT INTO cars(name, date_of_added) VALUES (?, ?)')
+        query.bindValue(0,carname)
+        query.bindValue(1,current_date.toString('yyyy-MM-dd'))
+        if not query.exec():
+            print(query.lastError().text())
+            return False
+        else:
+            query.finish()
+            return True
+
+    def add_income_in_db(self, car_id, income, hours):
+        current_date = QDate.currentDate().toString('yyyy-MM-dd')
+        print(car_id, income, hours, current_date)
+        query = QSqlQuery()
+        query.prepare('INSERT INTO income(car_id, amount_profit, hours_quantity, rent_day) VALUES (?, ?, ?, ?)')
+        query.bindValue(0,car_id)
+        query.bindValue(1,income)
+        query.bindValue(2,hours)
+        query.bindValue(3,current_date)
+        if not query.exec_():
+            print(query.lastError().text())
+            return False
+        else:
+            query.finish()
+            return True
         
+    def get_car_list(self):
+        car_list=[]
+        query = QSqlQuery()
+        query.prepare('SELECT name FROM cars')
+        if query.exec():
+            while query.next():
+                car_name = query.value(0)
+                car_list.append(car_name)
+        else:
+            print("Error executing query:", query.lastError().text())
+        print(car_list)
+        query.finish()  
+        return car_list
+    
+    def get_car_id(self, car_name):
+        conn = sqlite3.connect('dates.db')
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT id FROM cars WHERE name = ?", (car_name,))
+        car_id = cursor.fetchone()
+        conn.close()
+        if car_id:
+            return car_id[0]
+        else:
+            print('Car id - empty')
+            return None
+
+
+
     def closeConnection(self):
         self.db.close()
         print('Соединение закрыто')
--- mainwin.py
+++ mainwin.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt
 from gui.mainwindow import Ui_MainWindow
 from gui.addcar import Ui_AddCar
 from gui.addincome import Ui_AddIncome
@@ -11,11 +12,13 @@ class AddCarWindow(QMainWindow, Ui_AddCar):
         self.setupUi(self)
         self.car_name_cancel_button.clicked.connect(self.close)
 
+
 class AddIncomeWindow(QMainWindow, Ui_AddIncome):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.cancel_income_button.clicked.connect(self.close)
+
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -40,17 +43,57 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.cars_add.clicked.connect(self.open_add_car_window)
         self.income_add_button.clicked.connect(self.open_add_income_window)
 
+
     def income_page_open(self):
         self.stackedWidget.setCurrentWidget(self.income_page)
 
     def cars_page_open(self):
         self.stackedWidget.setCurrentWidget(self.cars_page)
+        self.refresh_car_list()
+
 
     def open_add_car_window(self):
         self.addCarWindow = AddCarWindow()
+        self.addCarWindow.car_name_add_button.clicked.connect(self.add_car)
         self.addCarWindow.show()
+
+    def refresh_car_list(self):
+        cars = self.connect.get_car_list()
+        for car in cars:
+            exists = False
+            for i in range(self.cars_list.count()):
+                item = self.cars_list.item(i)
+                if item.text() == car:
+                    exists = True
+                    break
+            if not exists:
+                self.cars_list.addItem(car)
 
     def open_add_income_window(self):
         self.addIncomeWindow = AddIncomeWindow()
+        cars = self.connect.get_car_list()
+        for car in cars:
+            self.addIncomeWindow.cars_combobox.addItem(car)
+        self.addIncomeWindow.add_income_button.clicked.connect(self.add_income)
         self.addIncomeWindow.show()
+
+    def add_car(self):
+        carname = self.addCarWindow.car_name_edit.text().strip()
+        if not carname == '':  
+            if self.connect.add_car_in_db(carname):
+                print('Add new line in table(cars)')
+                self.addCarWindow.close()
+                self.refresh_car_list()
+        else:
+            print('Поля ввода пустое!')
+
+    def add_income(self):
+        car_name = self.addIncomeWindow.cars_combobox.currentText().strip()
+        income = self.addIncomeWindow.income_income_edit.text()
+        hours = self.addIncomeWindow.time_income_edit.text()
+        car_id = self.connect.get_car_id(car_name)
+        if self.connect.add_income_in_db(car_id, income, hours):
+            print('Income was added in table')
+            self.addIncomeWindow.close()
+
     
